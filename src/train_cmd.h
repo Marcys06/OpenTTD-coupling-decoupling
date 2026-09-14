1719458aa17bce0456f87dc68ffc396132df5fc0
Marcys06 <marcelcieslik6@gmail.com>
2026-09-14 19:50:10 +0200

    0.0.3: Register coupling and decoupling command traits

--- src/train_cmd.h
+++ src/train_cmd.h
@@ -29,11 +29,9 @@ CommandCost CmdStationCouplingOrForceProceed(DoCommandFlags flags, VehicleID veh
 
 DEF_CMD_TUPLE_LT (Commands::MoveRailVehicle,          CmdMoveRailVehicle,           {}, CommandType::VehicleConstruction, CmdDataT<VehicleID, VehicleID, MoveRailVehicleFlags>)
 DEF_CMD_TUPLE_LT (Commands::ForceTrainProceed,        CmdStationCouplingOrForceProceed, {}, CommandType::VehicleManagement, CmdDataT<VehicleID>)
+DEF_CMD_TUPLE_LT (Commands::DecoupleTrain,            CmdDecoupleTrain,              {}, CommandType::VehicleManagement, CmdDataT<VehicleID>)
 DEF_CMD_TUPLE_LT (Commands::ReverseTrainDirection,    CmdReverseTrainDirection,     {}, CommandType::VehicleManagement,   CmdDataT<VehicleID, bool>)
 DEF_CMD_TUPLE_LT (Commands::SetTrainSpeedRestriction, CmdSetTrainSpeedRestriction, {}, CommandType::VehicleManagement,   CmdDataT<VehicleID, uint16_t>)
-
-/* Prototype GUI name: use the existing vehicle-management command slot. */
-#define DecoupleTrain ForceTrainProceed
 
 inline bool DetachTrainWagonChain(Train *part)
 {
