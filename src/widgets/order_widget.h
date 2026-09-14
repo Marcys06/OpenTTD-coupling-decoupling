cc518df3137a4d79b72cf0be2f1f96872e7c401a
Marcys06 <marcelcieslik6@gmail.com>
2026-09-14 21:03:54 +0200

    0.0.3: Add coupling order widget

--- src/widgets/order_widget.h
+++ src/widgets/order_widget.h
@@ -26,6 +26,7 @@ enum OrderWidgets : WidgetID {
 	WID_O_NON_STOP,                  ///< Goto non-stop to destination.
 	WID_O_DEPOT_UNBUNCHING,          ///< Toggle unbunching.
 	WID_O_GOTO,                      ///< Goto destination.
+	WID_O_COUPLING,                  ///< Add a train coupling/decoupling order.
 	WID_O_FULL_LOAD,                 ///< Select full load.
 	WID_O_UNLOAD,                    ///< Select unload.
 	WID_O_REFIT,                     ///< Select refit.
@@ -38,40 +39,40 @@ enum OrderWidgets : WidgetID {
 	WID_O_COND_CARGO,                ///< Choose condition cargo.
 	WID_O_COND_AUX_CARGO,            ///< Choose condition cargo.
 	WID_O_COND_SLOT,                 ///< Choose condition slot.
-	WID_O_COND_SLOT_GROUP,           ///< Choose condition slot group.
-	WID_O_COND_COUNTER,              ///< Choose condition counter.
+	WID_O_COND_SLOT_GROUP,            ///< Choose condition slot group.
+	WID_O_COND_COUNTER,              ///< Choose counter operation.
 	WID_O_COND_TIME_DATE,            ///< Choose time/date value.
-	WID_O_COND_TIMETABLE,            ///< Choose timetable value.
-	WID_O_COND_SCHED_SELECT,         ///< Choose scheduled dispatch schedule.
-	WID_O_COND_SCHED_VALUE,          ///< Choose scheduled dispatch value.
+	WID_O_COND_TIMETABLE,             ///< Choose timetable value.
+	WID_O_COND_SCHED_SELECT,          ///< Choose scheduled dispatch schedule.
+	WID_O_COND_SCHED_VALUE,           ///< Choose scheduled dispatch value.
 	WID_O_COND_AUX_VIA,              ///< Condition via button.
-	WID_O_COND_SCHED_TEST,           ///< Choose scheduled dispatch test.
-	WID_O_COND_AUX_STATION,          ///< Condition station button.
-	WID_O_COND_AUX_REFIT_MODE,       ///< Condition refit mode button.
+	WID_O_COND_SCHED_TEST,            ///< Choose scheduled dispatch test.
+	WID_O_COND_AUX_STATION,           ///< Condition station button.
+	WID_O_COND_AUX_REFIT_MODE,        ///< Condition refit mode button.
 	WID_O_SLOT,                      ///< Choose slot to try acquire/release.
 	WID_O_COUNTER_OP,                ///< Choose counter operation.
-	WID_O_CHANGE_COUNTER,            ///< Choose counter to change.
-	WID_O_COUNTER_VALUE,             ///< Choose counter value.
-	WID_O_TEXT_LABEL,                ///< Choose text label.
-	WID_O_DEPARTURE_VIA_TYPE,        ///< Choose departure board via subtype.
-	WID_O_SEL_COND_VALUE,            ///< Widget for conditional value or conditional cargo type.
-	WID_O_SEL_COND_AUX,              ///< Widget for auxiliary conditional cargo type. WID_O_SEL_COND_AUX... IDs must be sequential.
-	WID_O_SEL_COND_AUX2,             ///< Widget for auxiliary conditional via button.
-	WID_O_SEL_COND_AUX3,             ///< Widget for auxiliary conditional station button.
-	WID_O_SEL_COND_AUX4,             ///< Widget for auxiliary conditional refit mode button.
-	WID_O_SEL_MGMT,                  ///< Widget for management buttons.
-	WID_O_SEL_TOP_LEFT,              ///< #NWID_SELECTION widget for left part of the top row of the 'your train' order window.
-	WID_O_SEL_TOP_MIDDLE,            ///< #NWID_SELECTION widget for middle part of the top row of the 'your train' order window.
-	WID_O_SEL_TOP_RIGHT,             ///< #NWID_SELECTION widget for right part of the top row of the 'your train' order window.
-	WID_O_SEL_TOP_ROW_GROUNDVEHICLE, ///< #NWID_SELECTION widget for the top row of the 'your train' order window.
-	WID_O_SEL_TOP_ROW,               ///< #NWID_SELECTION widget for the top row of the 'your non-trains' order window.
-	WID_O_SEL_BOTTOM_MIDDLE,         ///< #NWID_SELECTION widget for the middle part of the bottom row of the 'your train' order window.
-	WID_O_SEL_SHARED,                ///< #NWID_SELECTION widget for WID_O_SHARED_ORDER_LIST and WID_O_ADD_VEH_GROUP
-	WID_O_SHARED_ORDER_LIST,         ///< Open list of shared vehicles.
-	WID_O_ADD_VEH_GROUP,             ///< Add single vehicle to new group button.
-	WID_O_SEL_OCCUPANCY,             ///< #NWID_SELECTION widget for the occupancy list panel.
-	WID_O_OCCUPANCY_LIST,            ///< Occupancy list panel.
-	WID_O_OCCUPANCY_TOGGLE,          ///< Toggle display of occupancy measures.
+	WID_O_CHANGE_COUNTER,             ///< Choose counter to change.
+	WID_O_COUNTER_VALUE,              ///< Choose counter value.
+	WID_O_TEXT_LABEL,                 ///< Choose text label.
+	WID_O_DEPARTURE_VIA_TYPE,         ///< Choose departure board via subtype.
+	WID_O_SEL_COND_VALUE,             ///< Widget for conditional value or conditional cargo type.
+	WID_O_SEL_COND_AUX,               ///< Widget for auxiliary conditional cargo type. WID_O_SEL_COND_AUX... IDs must be sequential.
+	WID_O_SEL_COND_AUX2,              ///< Widget for auxiliary conditional via button.
+	WID_O_SEL_COND_AUX3,              ///< Widget for auxiliary conditional station button.
+	WID_O_SEL_COND_AUX4,              ///< Widget for auxiliary conditional refit mode button.
+	WID_O_SEL_MGMT,                   ///< Widget for management buttons.
+	WID_O_SEL_TOP_LEFT,               ///< #NWID_SELECTION widget for left part of the top row of the 'your train' order window.
+	WID_O_SEL_TOP_MIDDLE,             ///< #NWID_SELECTION widget for middle part of the top row of the 'your train' order window.
+	WID_O_SEL_TOP_RIGHT,              ///< #NWID_SELECTION widget for right part of the top row of the 'your train' order window.
+	WID_O_SEL_TOP_ROW_GROUNDVEHICLE,  ///< #NWID_SELECTION widget for the top row of the 'your train' order window.
+	WID_O_SEL_TOP_ROW,                ///< #NWID_SELECTION widget for the top row of the 'your non-trains' order window.
+	WID_O_SEL_BOTTOM_MIDDLE,          ///< #NWID_SELECTION widget for the middle part of the bottom row of the 'your train' order window.
+	WID_O_SEL_SHARED,                 ///< #NWID_SELECTION widget for WID_O_SHARED_ORDER_LIST and WID_O_ADD_VEH_GROUP
+	WID_O_SHARED_ORDER_LIST,           ///< Open list of shared vehicles.
+	WID_O_ADD_VEH_GROUP,              ///< Add single vehicle to new group button.
+	WID_O_SEL_OCCUPANCY,               ///< #NWID_SELECTION widget for the occupancy list panel.
+	WID_O_OCCUPANCY_LIST,              ///< Occupancy list panel.
+	WID_O_OCCUPANCY_TOGGLE,            ///< Toggle display of occupancy measures.
 };
 
 /** Widgets of the #CargoTypeOrdersWindow class. */
